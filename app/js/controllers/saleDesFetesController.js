c914bc24bd69e6f0def59f5213d583acaf1f7231
Wolfy <s.leheup@gmail.com>
2017-04-24 17:01:51 +0200

    delete bootstrap in vendor. new modal materialize working fine

--- app/js/controllers/saleDesFetesController.js
+++ app/js/controllers/saleDesFetesController.js
@@ -2,13 +2,16 @@ angular.module('app')
     .controller('SDFController', function($scope, SDFService) {
         $scope.sallesDesFetes = [];
         $scope.sdf = {};
-        $('#myModal').on('shown.bs.modal', function() {
-            $('#myInput').focus();
-        });
 
-        $scope.modal = function(index) {
-            $scope.sdf = $scope.sallesDesFetes[index];
-        };
+          
+
+          $(document).ready(function() {
+              // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+              $('.modal').modal();
+
+          });
+
+
 
         SDFService.getAll().then(function(res) {
             $scope.sallesDesFetes = res.data;
@@ -49,15 +52,15 @@ angular.module('app')
         };
 
         $scope.deleteSDF = function(sdf) {
-            SDFService.delete(sdf._id).then(function(res){
-              console.log("delete succeed");
-              SDFService.getAll().then(function(res) {
-                  $scope.sallesDesFetes = res.data;
-                  console.log($scope.sallesDesFetes);
-              });
+            SDFService.delete(sdf._id).then(function(res) {
+                console.log("delete succeed");
+                SDFService.getAll().then(function(res) {
+                    $scope.sallesDesFetes = res.data;
+                    console.log($scope.sallesDesFetes);
+                });
 
             }, function(err) {
-              console.log("Delete failed");
-          });
+                console.log("Delete failed");
+            });
         };
     });
