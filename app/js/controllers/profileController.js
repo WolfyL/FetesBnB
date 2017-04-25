8e8b884e5e9410259254f7498c0b1ef5e916115f
olivier-goy <olivier.goy.37@gmail.com>
2017-04-25 15:54:51 +0200

    add update for modified profil

--- app/js/controllers/profileController.js
+++ app/js/controllers/profileController.js
@@ -4,8 +4,27 @@ angular.module('app')
             $scope.user = res.data;
             console.log($scope.user);
         });
+      var id = CurrentUser.user()._id;
+      $scope.user = [];
+
+      $scope.changeProfil = function () {
+
+        var user = {
+          firstName: $scope.user.firstName,
+          lastName: $scope.user.lastName,
+          email: $scope.user.email
+        };
+
+        if($scope.user.firstName.length > 0 && $scope.user.lastName.length > 0 && $scope.user.email) {
+          UserService.update(id, user).then(function(res) {
+            location.reload();
+          }, function (err) {
+            console.log(err);
+          });
+        }
 
 
+      };
 
 
           $(document).ready(function(){
