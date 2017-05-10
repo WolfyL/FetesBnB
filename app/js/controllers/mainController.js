102500f60a3fbccc54847f024e741811831982db
olivier-goy <olivier.goy.37@gmail.com>
2017-05-10 12:02:41 +0200

    change page home

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -2,4 +2,10 @@ angular.module('app')
     .controller('MainController', function($scope) {
         /* Here is your main controller */
 
+
+
+        
+        $(document).ready(function() {
+            $('select').material_select();
+        });
     });
