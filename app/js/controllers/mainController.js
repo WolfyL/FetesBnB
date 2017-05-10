ee9908a06e70fa28adaee7613aab689127e19069
olivier-goy <olivier.goy.37@gmail.com>
2017-05-10 15:01:55 +0200

    style for page home modified

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,9 +1,6 @@
 angular.module('app')
     .controller('MainController', function($scope) {
         /* Here is your main controller */
-
-
-
         
         $(document).ready(function() {
             $('select').material_select();
