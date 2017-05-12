e7fa9fe2672fd552883ab2632b0b0b06efc742b6
Raklaite <Raklaite@gmail.com>
2017-05-12 09:49:28 +0200

    correction controller

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,18 +1,25 @@
 angular.module('app')
-    .controller('MainController', function($scope, CurrentUser, UserService, SDFService) {
+    .controller('MainController', function($scope, CurrentUser, UserService, SDFService, NgMap) {
         /* Here is your main controller */
+        UserService.getOne(CurrentUser.user()._id).then(function(res) {
+              $scope.user = res.data;
+              console.log($scope.user.isAdmin);
+          });
+          SDFService.getAll().then(function(res){
+            console.log(res);
+            $scope.sallesDesFetes=res.data;
+          });
+          NgMap.getMap().then(function(map) {
+            $scope.map = map;
+          });
 
-
-        // UserService.getOne(CurrentUser.user()._id).then(function(res) {
-        //       $scope.user = res.data;
-        //       console.log($scope.user.isAdmin);
-        //   });
-        //   SDFService.getAll().then(function(res){
-        //     console.log(res);
-        //     $scope.sallesDesFetes=res.data;
-        //   });
-
-
+          $scope.showStore = function(evt, index) {
+              console.log('evt',evt);
+              console.log('index',index);
+              console.log('this',this);
+              $scope.sdf = $scope.sallesDesFetes[index];
+              $scope.map.showInfoWindow('window', this);
+          };
         $(document).ready(function() {
             $('select').material_select();
         });
