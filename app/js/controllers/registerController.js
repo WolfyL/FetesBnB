01c9afbe2b86046c257f267d62d0a02e6d290dba
olivier-goy <olivier.goy.37@gmail.com>
2017-05-15 09:56:27 +0200

    new page register and login with new function for record

--- app/js/controllers/registerController.js
+++ app/js/controllers/registerController.js
@@ -1,8 +1,21 @@
 angular.module('app')
     .controller('RegisterController', function($scope, $state, Auth) {
+
+        $scope.user = {};
         $scope.register = function() {
+
+          if ($scope.user.firstName != null && $scope.user.lastName != null && $scope.user.email != null && $scope.user.password != null) {
+
             Auth.register($scope.user).then(function() {
-                $state.go('anon.home');
+              $state.go('anon.home');
+            }, function (err) {
+              $scope.color = "red";
+              $scope.nop = "Mot de passe invalide ou mail incorrecte.";
             });
+            } else {
+              $scope.star = "*"
+              $scope.color = "red";
+              $scope.nopNop = "Tous les champs sont obligatoire.";
+          }
         };
     });
