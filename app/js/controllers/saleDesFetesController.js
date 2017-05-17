0b9e30eaf6c9b97dec683f6b553bf0121ce66b68
Wolfy <s.leheup@gmail.com>
2017-05-17 12:52:31 +0200

    ajout calendar modal + calendar propre a chaque salle

--- app/js/controllers/saleDesFetesController.js
+++ app/js/controllers/saleDesFetesController.js
@@ -5,25 +5,27 @@ angular.module('app')
         $scope.sdf = {};
         $scope.events = [];
 
+
         $(document).ready(function() {
             // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
             $('.modal').modal();
         });
 
-        $scope.eventCreatea = function(date) {
-            console.log($scope.eventStart);
-            // let hour = moment($scope.eventStart).format("hh:mm A").split(':')[0];
-            // let min = moment($scope.eventStart).format("hh:mm").split(':')[1];
-            let hourStart = moment($scope.eventStart).get('hour');
-            let minStart = moment($scope.eventStart).get('minute');
+        $scope.eventCreatea = function(indexSDF,date,event) {
+            console.log(event.eventStart);
+            console.log('index',indexSDF);
+            // let hour = moment($scope.event.eventStart).format("hh:mm A").split(':')[0];
+            // let min = moment($scope.event.eventStart).format("hh:mm").split(':')[1];
+            let hourStart = moment(event.eventStart).get('hour');
+            let minStart = moment(event.eventStart).get('minute');
             let startDate = moment(date).add(hourStart, 'h').add(minStart, 'm');
 
-            let hourEnd = moment($scope.eventEnd).get('hour');
-            let minEnd = moment($scope.eventEnd).get('minute');
-            let endDate = moment($scope.dayEnd).add(hourEnd, 'h').add(minEnd, 'm');
+            let hourEnd = moment(event.eventEnd).get('hour');
+            let minEnd = moment(event.eventEnd).get('minute');
+            let endDate = moment(event.dayEnd).add(hourEnd, 'h').add(minEnd, 'm');
 
-            console.log("eventEnd : ", $scope.eventEnd);
-            console.log("dayEnd : ", $scope.dayEnd);
+            console.log("event.eventEnd : ", event.eventEnd);
+            console.log("event.dayEnd : ", event.dayEnd);
 
             console.log("minEnd avant", minEnd, typeof(minEnd));
             if (minEnd === 0) {
@@ -31,47 +33,52 @@ angular.module('app')
               minEnd = '00';
             }
 
-            if ($scope.dayEnd !== undefined && $scope.eventEnd !== undefined && $scope.dayEnd !== '' && $scope.eventEnd !== '') {
-                $scope.events.push({
-                    title: $scope.eventTitle + " fini à:" + hourEnd + ':' + minEnd,
+            if (event.dayEnd !== undefined && event.eventEnd !== undefined && event.dayEnd !== '' && event.eventEnd !== '') {
+                $scope.sallesDesFetes[indexSDF].events.push({
+                    title: event.eventTitle + " fini à:" + hourEnd + ':' + minEnd,
                     start: new Date(startDate),
-                    end: new Date($scope.dayEnd),
+                    end: new Date(event.dayEnd),
                     allDay: false
                 });
                 console.log("1");
-            } else if (($scope.dayEnd === undefined || $scope.dayEnd === '') && ($scope.eventEnd === undefined || $scope.eventEnd === '')) {
-                $scope.events.push({
-                    title: $scope.eventTitle,
+            } else if ((event.dayEnd === undefined || event.dayEnd === '') && (event.eventEnd === undefined || event.eventEnd === '')) {
+                  $scope.sallesDesFetes[indexSDF].events.push({
+                    title: event.eventTitle,
                     start: new Date(startDate),
                     allDay: false
                 });
                 console.log("2");
-            } else if ($scope.dayEnd === undefined || $scope.dayEnd === '') {
-                $scope.events.push({
-                    title: $scope.eventTitle + " fini à:" + hourEnd + ':' + minEnd,
+            } else if (event.dayEnd === undefined || event.dayEnd === '') {
+                  $scope.sallesDesFetes[indexSDF].events.push({
+                    title: event.eventTitle + " fini à:" + hourEnd + ':' + minEnd,
                     start: new Date(startDate),
                     allDay: false
                 });
                 console.log("3");
-            } else if ($scope.eventEnd === undefined || $scope.eventEnd === '') {
-                $scope.events.push({
-                    title: $scope.eventTitle,
+            } else if (event.eventEnd === undefined || event.eventEnd === '') {
+                  $scope.sallesDesFetes[indexSDF].events.push({
+                    title: event.eventTitle,
                     start: new Date(startDate),
-                    end: new Date($scope.dayEnd),
+                    end: new Date(event.dayEnd),
                     allDay: false
                 });
                 console.log("4");
             }
 
-            $scope.eventTitle = "";
-            $scope.eventStart = "";
-            $scope.dayEnd = "";
-            $scope.eventEnd = "";
+            event.eventTitle = "";
+            event.eventStart = "";
+            event.dayEnd = "";
+            event.eventEnd = "";
         };
 
         SDFService.getAll().then(function(res) {
             $scope.sallesDesFetes = res.data;
             console.log($scope.sallesDesFetes);
+            for(var index in $scope.sallesDesFetes) {
+              if ($scope.sallesDesFetes[index].events === undefined) {
+                $scope.sallesDesFetes[index].events = [];
+              }
+            }
         });
 
         $scope.addSDF = function() {
