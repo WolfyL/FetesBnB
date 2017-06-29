4e5c1fb5fe61ff775b11054dcd878952f06c0aa1
Raklaite <Raklaite@gmail.com>
2017-06-29 16:24:31 +0200

    modif model

--- server/api/models/salleDesFetes.js
+++ server/api/models/salleDesFetes.js
@@ -8,53 +8,51 @@ import removeAccents from 'remove-accents';
 
 const sdfSchema = new mongoose.Schema({
 
-  name: {
-    type: String,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  postalCode: {
-    type: String,
-    required: true,
-  },
-  adress: {
-    type: String,
-    required: true,
-  },
-  capacity: {
-    type: Number,
-    required: true,
-  },
-  surface: {
-    type: Number,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  text: {
-    type: String,
-    required: true,
-  },
-  coordo: {
-    lat: String,
-    lng: String
-  },
-  evenement: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Event'
-  }],
-  handler: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }
+    name: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    postalCode: {
+        type: String,
+        required: true,
+    },
+    adress: {
+        type: String,
+        required: true,
+    },
+    capacity: {
+        type: Number,
+        required: true,
+    },
+    surface: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    coordo: {
+        lat: String,
+        lng: String
+    },
+    evenement: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event'
+    }],
+    handler: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    image: [String]
 });
 
 
@@ -129,152 +127,153 @@ export default class SDF {
             });
     }
 
-  findById(req, res) {
-    model.findById(req.params.id)
-      .populate('evenement')
-      .exec((err, salleDesFetes) => {
-        if (err || !salleDesFetes) {
-          res.status(500).json(err);
-        } else {
-          res.json(salleDesFetes);
-        }
-      });
-  }
+    findById(req, res) {
+        model.findById(req.params.id)
+            .populate('evenement')
+            .exec((err, salleDesFetes) => {
+                if (err || !salleDesFetes) {
+                    res.status(500).json(err);
+                } else {
+                    res.json(salleDesFetes);
+                }
+            });
+    }
 
-  getMySDF(req, res) {
-    model.find({
-        handler: req.params.handler
-      },
-      (err, salleDesFetes) => {
-        // console.log(req.body.handler);
-        if (err || !salleDesFetes) {
-          res.status(500).send(err.message);
-        } else {
-          res.json(salleDesFetes);
-        }
-      }
-    );
-  }
+    getMySDF(req, res) {
+        model.find({
+                handler: req.params.handler
+            },
+            (err, salleDesFetes) => {
+                // console.log(req.body.handler);
+                if (err || !salleDesFetes) {
+                    res.status(500).send(err.message);
+                } else {
+                    res.json(salleDesFetes);
+                }
+            }
+        );
+    }
 
-  getHandler(req, res) {
-    model.findById(req.params.id)
-      .populate({
-        path: 'handler',
-        select: 'email'
-      })
-      .exec((err, salleDesFetes) => {
-        if (err || !salleDesFetes) {
-          res.status(500).json(err);
-        } else {
-          res.json(salleDesFetes);
-        }
-      });
-  }
-  updateImg(req, res) {
-    console.log('body', req.body);
-    model.findByIdAndUpdate({
-        _id: req.params.id
-      }, req.body, {
-        new: true
-      },
-      (err, salleDesFetes) => {
-        if (err || !salleDesFetes) {
-          res.status(500).send(err.message);
-        } else {
-          res.json({
-            success: true,
-            salleDesFetes: salleDesFetes,
-          });
-        }
-      });
-  }
-  getImg(req, res) {
-    console.log('body', req.body);
-    model.findByIdAndUpdate({
-        _id: req.params.image
-      }, req.body, {
-        new: true
-      },
-      (err, salleDesFetes) => {
-        if (err || !salleDesFetes) {
-          res.status(500).send(err.message);
-        } else {
-          res.json({
-            success: true,
-            salleDesFetes: salleDesFetes,
-          });
-        }
-      });
-  }
+    getHandler(req, res) {
+        model.findById(req.params.id)
+            .populate({
+                path: 'handler',
+                select: 'email'
+            })
+            .exec((err, salleDesFetes) => {
+                if (err || !salleDesFetes) {
+                    res.status(500).json(err);
+                } else {
+                    res.json(salleDesFetes);
+                }
+            });
+    }
+    updateImg(req, res) {
+        console.log('body', req.body);
+        model.findByIdAndUpdate(
+            req.params.id, {
+                $push: { image: req.body.image }
+            }, {
+                upsert: true,
+            },
+            (err, salleDesFetes) => {
+                if (err || !salleDesFetes) {
+                    res.status(500).send(err.message);
+                } else {
+                    res.json({
+                        success: true,
+                        salleDesFetes: salleDesFetes,
+                    });
+                }
+            });
+    }
+    getImg(req, res) {
+        console.log('body', req.body);
+        model.findByIdAndUpdate({
+                _id: req.params.image
+            }, req.body, {
+                new: true
+            },
+            (err, salleDesFetes) => {
+                if (err || !salleDesFetes) {
+                    res.status(500).send(err.message);
+                } else {
+                    res.json({
+                        success: true,
+                        salleDesFetes: salleDesFetes,
+                    });
+                }
+            });
+    }
 
     create(req, res) {
-    let coordo;
-    model.create(req.body,
-      (err, salleDesFetes) => {
-        if (err) {
-          res.status(500).send(err.message);
-        } else
-          //adress et city puis france
-          request('https://maps.googleapis.com/maps/api/geocode/json?address=' + removeAccents(salleDesFetes.adress) + salleDesFetes.postalCode + removeAccents(salleDesFetes.city) + '&key=AIzaSyAwtHS2XSIYvSChTHcQPyf1Fs3K8GPSs7w', function(error, result, body) {
-            var donnee = JSON.parse(result.body);
-            coordo = {
-              lat: donnee.results[0].geometry.location.lat,
-              lng: donnee.results[0].geometry.location.lng
-            };
-            model.findOneAndUpdate({
-              _id: salleDesFetes._id
-            }, {
-              coordo: coordo
-            }, {
-              upsert: true,
-              new: true
-            }, (err, salleDesFetes) => {
-              console.log("JE SUIS DANS L UPDATE");
-              console.log(coordo);
-              if (err || !salleDesFetes) {
-                res.status(500).send(err.message);
-              } else {
-                res.json({
-                  success: true,
-                  salleDesFetes: salleDesFetes,
+        let coordo;
+        model.create(req.body,
+            (err, salleDesFetes) => {
+                if (err) {
+                    res.status(500).send(err.message);
+                } else
+                //adress et city puis france
+                    request('https://maps.googleapis.com/maps/api/geocode/json?address=' + removeAccents(salleDesFetes.adress) + salleDesFetes.postalCode + removeAccents(salleDesFetes.city) + '&key=AIzaSyAwtHS2XSIYvSChTHcQPyf1Fs3K8GPSs7w', function(error, result, body) {
+                    var donnee = JSON.parse(result.body);
+                    coordo = {
+                        lat: donnee.results[0].geometry.location.lat,
+                        lng: donnee.results[0].geometry.location.lng
+                    };
+                    model.findOneAndUpdate({
+                        _id: salleDesFetes._id
+                    }, {
+                        coordo: coordo
+                    }, {
+                        upsert: true,
+                        new: true
+                    }, (err, salleDesFetes) => {
+                        console.log("JE SUIS DANS L UPDATE");
+                        console.log(coordo);
+                        if (err || !salleDesFetes) {
+                            res.status(500).send(err.message);
+                        } else {
+                            res.json({
+                                success: true,
+                                salleDesFetes: salleDesFetes,
+                            });
+                        }
+                    });
+                    console.log("COORDO : ", coordo);
                 });
-              }
             });
-            console.log("COORDO : ", coordo);
-          });
-      });
-  }
-  update(req, res) {
-    console.log('body', req.body);
-    model.findByIdAndUpdate({
-        _id: req.params.id
+    }
+    update(req, res) {
+        console.log('body', req.body);
+        model.findByIdAndUpdate({
+                _id: req.params.id
 
-      }, {
-        $addToSet: {
-          evenement: req.body._id
-        }
-      }, {
-        new: true
-      },
-      (err, salleDesFetes) => {
-        if (err || !salleDesFetes) {
-          res.status(500).send(err.message);
-        } else {
-          res.json({
-            success: true,
-            salleDesFetes: salleDesFetes,
-          });
-        }
-      });
-}
+            }, {
+                $addToSet: {
+                    evenement: req.body._id
+                }
+            }, {
+                new: true
+            },
+            (err, salleDesFetes) => {
+                if (err || !salleDesFetes) {
+                    res.status(500).send(err.message);
+                } else {
+                    res.json({
+                        success: true,
+                        salleDesFetes: salleDesFetes,
+                    });
+                }
+            });
+    }
 
-  delete(req, res) {
-    model.findByIdAndRemove(req.params.id, (err) => {
-      if (err) {
-        res.status(500).send(err.message);
-      } else {
-        res.sendStatus(200);
-      }
-    });
-  }
+    delete(req, res) {
+        model.findByIdAndRemove(req.params.id, (err) => {
+            if (err) {
+                res.status(500).send(err.message);
+            } else {
+                res.sendStatus(200);
+            }
+        });
+    }
 }
