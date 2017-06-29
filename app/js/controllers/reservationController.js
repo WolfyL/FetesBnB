526df541d0cb4b482164a2dbcfbd564cd11a5a25
olivier-goy <olivier.goy.37@gmail.com>
2017-06-29 16:14:03 +0200

    style for reservation page

--- app/js/controllers/reservationController.js
+++ app/js/controllers/reservationController.js
@@ -1,30 +1,39 @@
 angular.module('app')
   .controller('ResaController', function($scope, CurrentUser, UserService, SDFService, $state) {
-
+    moment.locale('fr');
     // console.log($state.params);
 
-    SDFService.getOne($state.params.sdf).then(function(res){
+    SDFService.getOne($state.params.sdf).then(function(res) {
       // console.log(res.data);
       $scope.nomSalle = res.data.name;
     });
-
+    var link;
     $scope.sendMail = function(resaDateStart, resaDateEnd, precisionUser) {
+
       SDFService.getSDFHandler($state.params.sdf).then(function(res) {
         console.log("resdata: ", res.data);
         $scope.currentHandler = res.data.handler.email;
-        $scope.resaDateStart = resaDateStart;
-        $scope.resaDateEnd = resaDateEnd;
+        $scope.resaDateStart = moment(resaDateStart).format("LL");
+        $scope.resaDateEnd = moment(resaDateEnd).format("LL");
         $scope.precisionUser = precisionUser;
 
         console.log($scope.currentHandler, "= res.data.handler.email;",
-        $scope.resaDateStart, "= resaDateStart;",
-        $scope.resaDateEnd, "= resaDateEnd;", $scope.precisionUser," = precisionUser;");
+          $scope.resaDateStart, "= resaDateStart;",
+          $scope.resaDateEnd, "= resaDateEnd;", $scope.precisionUser, " = precisionUser;");
 
-        var link = "mailto:" + $scope.currentHandler +
-          "?subject=" + escape("Réservation de la salle" + $scope.nomSalle) +
-          "&body=" + escape("Nous souhaiterions réserver la salle" + $scope.nomSalle +
-          " du " + $scope.resaDateStart + " au " + $scope.resaDateEnd + ". -Précision de l'utilisateur : " + $scope.precisionUser);
-        window.location.href = link;
+        if ($scope.precisionUser !== undefined) {
+          link = "mailto:" + $scope.currentHandler +
+            "?subject=" + "Réservation de la salle " + $scope.nomSalle +
+            "&body=" + "Nous souhaiterions réserver la salle " + $scope.nomSalle +
+            " du " + $scope.resaDateStart + " au " + $scope.resaDateEnd + "." + " Précision de l'utilisateur : " + $scope.precisionUser;
+          window.location.href = link;
+        } else {
+          link = "mailto:" + $scope.currentHandler +
+            "?subject=" + "Réservation de la salle " + $scope.nomSalle +
+            "&body=" + "Nous souhaiterions réserver la salle " + $scope.nomSalle +
+            " du " + $scope.resaDateStart + " au " + $scope.resaDateEnd + ".";
+          window.location.href = link;
+        }
       });
     };
 
