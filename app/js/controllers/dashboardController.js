e8e5dc4dc979e3bc8cdfc754a8826402109f8759
Wolfy <s.leheup@gmail.com>
2017-07-05 13:18:42 +0200

    try fix fav. Need test 1st fav

--- app/js/controllers/dashboardController.js
+++ app/js/controllers/dashboardController.js
@@ -1,29 +1,25 @@
 angular.module('app')
-    .controller('DashboardController', function($scope, CurrentUser, UserService, SDFService) {
-        $scope.user = CurrentUser.user();
-        $scope.likeds = [];
+  .controller('DashboardController', function($scope, CurrentUser, UserService, SDFService) {
+    $scope.user = CurrentUser.user();
+    $scope.likeds = [];
 
-        SDFService.getAll().then(function(res) {
-            $scope.sallesDesFetes = res.data;
-        });
-        SDFService.getImg().then(function(res) {
-        });
-
-        function loadLikeds() {
-            UserService.getOne(CurrentUser.user()._id).then(function(res) {
-                $scope.likeds = (res.data.liked);
-                console.log($scope.likeds);
-            });
-        }
-        loadLikeds();
-
-        $scope.delFav = function(salleDesFetes) {
-            UserService.delFav($scope.user._id, salleDesFetes).then(function(res) {
-                loadLikeds();
-            });
-
-
-
-        };
-
+    SDFService.getAll().then(function(res) {
+      $scope.sallesDesFetes = res.data;
     });
+    SDFService.getImg().then(function(res) {});
+
+    function loadLikeds() {
+      UserService.getOne(CurrentUser.user()._id).then(function(res) {
+        $scope.likeds = res.data.liked;
+        // console.log($scope.likeds);
+      });
+    }
+    loadLikeds();
+
+    $scope.delFav = function(id) {
+      console.log(id);
+      UserService.delFav($scope.user._id, id).then(function(res) {
+        loadLikeds();
+      });
+    };
+  });
